160edeeb341a0c5dfd0c105dd96748694d4c33a3
HelloFZ <869640364@qq.com>
2017-05-07 19:32:38 +0800

    Signed-off-by: HelloFZ <869640364@qq.com>

---
+++ Game/app/src/main/java/com/example/administrator/game/wuziview.java
@@ -0,0 +1,21 @@
+package com.example.administrator.game;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/5/2 0007.
+ */
+
+public class wuziview extends View {
+
+    public wuziview(Context context) {
+        this(context,null);
+    }
+
+    public wuziview(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+
+    }
+}
